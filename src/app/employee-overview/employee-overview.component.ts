369b3bd6c9d182cdfc86c597eb357d59a0ad9ffe
Leon Kunkel <leon.kunkel@wamoco.de>
2026-01-15 10:27:53 +0100

    employee overview component added.

---
+++ src/app/employee-overview/employee-overview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-overview',
+  standalone: true,
+  imports: [],
+  templateUrl: './employee-overview.component.html',
+  styleUrl: './employee-overview.component.css'
+})
+export class EmployeeOverviewComponent {
+
+}
